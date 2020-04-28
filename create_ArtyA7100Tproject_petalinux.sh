c45f672a489a7530d0bcdcbee437549e3697bdef
Chris Peck <chris.peck@internode.on.net>
2020-04-28 13:16:50 +0930

    petalinux build script added
    Will build but currently gives error “ERROR: Size of bitstream "/home/peckc/artytest/artytest.petalinux/images/linux/u-boot.elf" is 3493928 larger than the boot partition size 0x40000.”
    Will have to modify boot partition size to fix

---
+++ create_ArtyA7100Tproject_petalinux.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+petalinux-create --type project --template microblaze --name artytest.petalinux
+mkdir -p artytest.petalinux/images/linux/sysroot_dir
+cp artytest.vitis/design_1_wrapper.xsa artytest.petalinux/
+cd artytest.petalinux
+petalinux-config --get-hw-description
+petalinux-config -c u-boot
+petalinux-config -c kernel
+petalinux-config -c rootfs
+petalinux-build
+cd images/linux/
+petalinux-package --boot --fsbl fs-boot.elf --u-boot u-boot.elf --mmi ../../../artytest.runs/impl_1/design_1_wrapper.mmi --fpga ../../../artytest.runs/impl_1/design_1_wrapper.bit
