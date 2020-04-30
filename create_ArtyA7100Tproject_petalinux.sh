e2a15440e9e8e383420bb16bac48b27dc7ff8386
Chris Peck <chris.peck@internode.on.net>
2020-04-30 20:31:51 +0930

    work in progress

--- create_ArtyA7100Tproject_petalinux.sh
+++ create_ArtyA7100Tproject_petalinux.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#TODO source environments
+
 petalinux-create --type project --template microblaze --name artytest.petalinux
 mkdir -p artytest.petalinux/images/linux/sysroot_dir
 cp artytest.vitis/design_1_wrapper.xsa artytest.petalinux/
@@ -9,4 +11,4 @@ petalinux-config -c kernel
 petalinux-config -c rootfs
 petalinux-build
 cd images/linux/
-petalinux-package --boot --fsbl fs-boot.elf --u-boot u-boot.elf --mmi ../../../artytest.runs/impl_1/design_1_wrapper.mmi --fpga ../../../artytest.runs/impl_1/design_1_wrapper.bit
+petalinux-package --boot --fsbl fs-boot.elf --u-boot u-boot.elf --mmi ../../../artytest.runs/impl_1/design_1_wrapper.mmi --fpga ../../../artytest.runs/impl_1/design_1_wrapper.bit --force --flash-size 16 --flash-intf SPIx4
